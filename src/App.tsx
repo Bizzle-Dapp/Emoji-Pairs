901506aa34cc5a303c5f4c806d32591395e39520
bizzle_dapp <lschofield1991@gmail.com>
2020-06-24 17:17:44 +0100

    removed unused using statements

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { DifficultySelect } from './Components/DifficultySelect';
 import { Game } from './Components/Game';
--- src/Components/Game.tsx
+++ src/Components/Game.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import '../App.css';
 import { Card, CardProps } from './Card';
-import { waitForElementToBeRemoved } from '@testing-library/react';
 
 enum Difficulty {
     None,
